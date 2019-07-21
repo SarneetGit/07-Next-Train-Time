d1f5893fc740b0f7fd659de727bda876199a9325
Sarneet Longia <sarneet.longia@gmail.com>
2019-07-21 16:15:24 -0400

    Committing final version for submission, added full functionality as per requirements and validation on military time

--- javascript.js
+++ javascript.js
@@ -22,6 +22,20 @@ time = "",
 frequency = "",
 placeHolder = "Coming Soon!"
 
+$("#time").keyup(function() {
+    let inp = $(this).val()
+    let re = new RegExp("^([0-1][0-9]|2[0-3]):([0-5][0-9])$")
+    if (re.test(inp)) {
+        $("#time").removeAttr("style")
+        $("#myButton").removeAttr("disabled")
+        console.log("Happy Path")
+    }
+    else {
+        $("#time").attr("style", "border: 2px solid red;")
+        $("#myButton").attr("disabled", "disabled")
+    }
+})
+
 $("#myButton").click(function(event) {
     event.preventDefault();
 
@@ -38,13 +52,41 @@ $("#myButton").click(function(event) {
     })
 })
 
+db.ref("/train").on("child_added", function(snapshot) {    
+    let a = moment()
+    let b = snapshot.val().Start.split(":")
+    let c = moment().hour(b[0]).minute(b[1])
+    let totalMinutes = ""
+    , trainsPassed = ""
+    , minutesRemaining = ""
+    , minutesShow = ""
+    , dateShow = ""
 
-db.ref("/train").on("child_added", function(snapshot) {
+    if (a >= c) {
+        totalMinutes = a.diff(c, "minutes")
+        trainsPassed = Math.floor(totalMinutes/parseInt(snapshot.val().Frequency))
+        minutesRemaining = totalMinutes - (trainsPassed * parseInt(snapshot.val().Frequency))
+        minutesShow = parseInt(snapshot.val().Frequency) - minutesRemaining 
+        dateShow = a.add(minutesShow, "m").format("H:mm A").replace(/N/g, "P").replace(/V/g, "A")
+        console.log(dateShow)
+    }
+
+    else {
+        minutesShow = c.diff(a, "minutes")
+        if (parseInt(b[0]) >= 12) {
+            dateShow = snapshot.val().Start + " PM"
+        }
+        else {
+            dateShow = snapshot.val().Start + " AM"
+        }
+    }
+    
     $("#table").append(`<tr>
                             <td>${snapshot.val().Train}</td>
                             <td>${snapshot.val().Destination}</td>
-                            <td>${snapshot.val().Start}</td>                            
-                            <td>${snapshot.val().Frequency}</td>
-                            <td>${placeHolder}</td>                                   
-                        </tr>`)    
-})
+                            <td>${snapshot.val().Frequency}</td>                            
+                            <td>${dateShow}</td>
+                            <td>${minutesShow}</td>                                   
+                        </tr>`) 
+
+})  
